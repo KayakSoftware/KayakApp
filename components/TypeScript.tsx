b3f9d38276fe3f3960646faa24a47d5e751f9f37
Ulrik Sandberg <UJSandberg@gmail.com>
2020-10-06 13:07:50 +0200

    Add typescript, got accelerometer data

---
+++ components/TypeScript.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const TypeScriptComponent = () => {
+    return (
+        <View>
+            <Text>I'm a typescript component</Text>
+        </View>
+    )
+}
+
+export default TypeScriptComponent;
---
+++ screens/HomeScreen.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+import Sensor from "../components/Sensor";
+
+const HomeScreen = () => {
+
+    const render = () => {
+        return <Sensor></Sensor>
+    }
+
+    return render();
+}
+
+export default HomeScreen;
